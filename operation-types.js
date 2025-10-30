b85e9a0fc0ea867c0ac2a04eaba64d41e5bfe817
CcphoK <flavoirkeswa@gmail.com>
2025-10-30 16:31:08 +0200

    JS into

---
+++ operation-types.js
@@ -0,0 +1,13 @@
+// TODO: Create a variable called number1 and store any number in it
+// TODO: Create a variable called number2 and store a different number in it
+// TODO: Convert number1 to a string and store it in a new variable
+// TODO: Add number1 and number2 and store the result in a variable called sum
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+// TODO: Print all results to the console
+let number1 = 18;
+let number2 = 15;
+let newNumber1 = number1.toString();
+let sum = number1 * number2;
+console.log("number1 as a string: " + number1);
+console.log("Sum: " = sum);
+console.log("Product: " + product);
